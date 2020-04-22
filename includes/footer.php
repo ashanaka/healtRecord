95cb7e1dc5066df23d3e2dcf19fe50f528922d3f
Ashan Shanaka <jpashanshanaka@gmail.com>
2020-04-22 14:30:12 +0530

    header and footer

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+        <div class="footer">
+            <p>All rights reserved by - Ashan Shanaka | Undergraduate at UCSC | Email - ashansjp@gmail.com</p>
+        </div>
+
+    </body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Health Record | Home</title>
+    <link rel="stylesheet" type="text/css" href="./css/header.css">
+    <link rel="stylesheet" type="text/css" href="./css/footer.css">
+</head>
+<body>
+
+<ul>
+  <li><a href="#home">Home</a></li>
+  <li><a href="#news">About</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li style="float:right"><a class="active" href="#about">Log In</a></li>
+  <li style="float:right"><a class="active" href="#about">Register</a></li>
+</ul>
